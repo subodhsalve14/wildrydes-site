d6911d317a3d77ca4d11e03d69963e4b49b3581f
Subodh Salve <107650129+subodhsalve14@users.noreply.github.com>
2025-04-22 23:34:31 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_58naSvvIG',
         userPoolClientId: '3s0evff4np4obvefnfal3najlk',
-        userPoolClientSecret: 'YOUR_CLIENT_SECRET_HERE', // Add this
+        userPoolClientSecret: '1ev6snon3talk5udoij66hoike4mb8aoo4tfjan4tvp25f4hejof',
         region: 'us-east-1'
     },
     api: {
