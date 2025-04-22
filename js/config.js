1597247f3a339895984c66d944a333470122a7ff
Subodh Salve <107650129+subodhsalve14@users.noreply.github.com>
2025-04-22 23:32:21 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_58naSvvIG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3s0evff4np4obvefnfal3najlk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_58naSvvIG',
+        userPoolClientId: '3s0evff4np4obvefnfal3najlk',
+        userPoolClientSecret: 'YOUR_CLIENT_SECRET_HERE', // Add this
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ''
     }
 };
